9766a75e7406210c46f2ccbdd2845be7603a22eb
Arsen Asatryan <arsen_asatryan@epam.com>
2023-11-02 16:24:05 +0400

    chenged app

--- src/main/java/com/example/cicd/CiCdApplication.java
+++ src/main/java/com/example/cicd/CiCdApplication.java
@@ -5,16 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CiCdApplication {
-
     public static void main(String[] args) {
-       int a = 1;
-        {
-            int b = 2+a;
-
-        }
-
-
-
         SpringApplication.run(CiCdApplication.class, args);
     }
 
