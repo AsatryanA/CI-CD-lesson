9b7b63e81c11e8aa3fec6c531753098720b4e674
Arsen Asatryan <arsen_asatryan@epam.com>
2023-11-02 18:39:18 +0400

    added tests

--- src/main/java/com/example/cicd/model/Student.java
+++ src/main/java/com/example/cicd/model/Student.java
@@ -1,10 +1,14 @@
 package com.example.cicd.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
