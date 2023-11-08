d44fa4577538d4e66ebdf3a1ca50e2320c60f634
Arsen Asatryan <arsen_asatryan@epam.com>
2023-11-09 00:44:13 +0400

    jjj

--- src/main/java/com/example/cicd/CiCdApplication.java
+++ src/main/java/com/example/cicd/CiCdApplication.java
@@ -2,11 +2,18 @@ package com.example.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CiCdApplication {
+public class CiCdApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(CiCdApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+        return builder.sources(CiCdApplication.class);
     }
 
 }
